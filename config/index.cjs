95fcc8a269f2262838909c35a3129c028619e25c
Acong3017 <120289213+Acong3017@users.noreply.github.com>
2022-12-11 15:58:35 +0800

    Create index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '胡萝卜',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'AKlnfs0LQX6w90xLCuLL1PKRWKJ9vomHouUp2yiXZ1w',
+      id: 'oiQAY6EJS_qh6IdxDx5XtkEw_V3Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'XcupTbvLTA1gIXtJTOAa3SGKHWsDJWx0Qt7j-B-fjmc',
+      useTemplateId: 'AKlnfs0LQX6w90xLCuLL1PKRWKJ9vomHouUp2yiXZ1w',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-29',
       festivals: [
