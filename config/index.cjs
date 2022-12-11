fadbf56d22d28a0ba8876e94b26aa1569b787bfd
Acong3017 <120289213+Acong3017@users.noreply.github.com>
2022-12-11 14:17:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆大人',
+      name: '给老婆大人请安啦！',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oiQAY6EJS_qh6IdxDx5XtkEw_V3Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'prtxaJlcHehQwAP0S0RCYEpeBVWb8H6MzAyzPqnpw7U',
+      useTemplateId: 'g1IC6fT0cTZTG2wp2giQf60c8cFyAijNicdd_3UX75o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-29',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'prtxaJlcHehQwAP0S0RCYEpeBVWb8H6MzAyzPqnpw7U',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oiQAY6EJS_qh6IdxDx5XtkEw_V3Q',
+      id: '',
     }
   ],
 
