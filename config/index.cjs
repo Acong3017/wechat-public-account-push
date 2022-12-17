0bba838ea8932edd65c5045888ed21899389a817
Acong3017 <120289213+Acong3017@users.noreply.github.com>
2022-12-17 11:11:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oiQAY6LlKoZwegOKz0taAPLPq3yc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'EAlgyuetB7T_Go_YDJlcrj-x3gKSSzjst3l7sejg_FU',
+      useTemplateId: 'TsrTCWVAYQtY38VkX3ynU2U6sT44EUUBV39othJpV10',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-29',
       festivals: [
